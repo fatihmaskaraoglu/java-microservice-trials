61ef6da0033f1623251d6704083211f0f9b6b939
fatihmaskaraoglu <fatihmaskaraoglu@gmail.com>
2023-06-14 23:38:47 +0300

    first commit for limit services

---
+++ limits-service/src/main/java/com/myapp/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,13 @@
+package com.myapp.microservices.limitsservice.controller;
+
+import com.myapp.microservices.limitsservice.bean.Limits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+    @GetMapping("/limits")
+    public Limits retrieveLimits(){
+        return new Limits(1,1000);
+    }
+}
