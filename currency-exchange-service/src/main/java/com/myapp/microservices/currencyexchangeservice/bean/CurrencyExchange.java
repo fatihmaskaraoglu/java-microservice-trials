9a0e5ffd40da44cf4496561e9849bf13a3269892
fatihmaskaraoglu <fatihmaskaraoglu@gmail.com>
2023-06-17 22:08:09 +0300

    Configure JPA and Initialized Data

--- currency-exchange-service/src/main/java/com/myapp/microservices/currencyexchangeservice/bean/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/myapp/microservices/currencyexchangeservice/bean/CurrencyExchange.java
@@ -1,5 +1,8 @@
 package com.myapp.microservices.currencyexchangeservice.bean;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,9 +10,13 @@ import java.math.BigDecimal;
 
 @Getter
 @Setter
+@Entity
 public class CurrencyExchange {
+    @Id
     private Long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
