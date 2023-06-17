1c2a6b1e14d2099c08fe8b80a57894e34aaf9034
fatihmaskaraoglu <fatihmaskaraoglu@gmail.com>
2023-06-17 23:07:22 +0300

    Invoking Currency Exchange from Currency Conversion Microservice

---
+++ currency-conversion-service/src/main/java/com/myapp/microservices/currencyconversionservice/bean/CurrencyConversion.java
@@ -0,0 +1,46 @@
+package com.myapp.microservices.currencyconversionservice.bean;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+
+@Getter
+@Setter
+public class CurrencyConversion {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private BigDecimal quantity;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
+
+    public CurrencyConversion() {
+    }
+
+    public CurrencyConversion(Long id, String from, String to, BigDecimal conversionMultiple, BigDecimal quantity, BigDecimal totalCalculatedAmount, String environment) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+        this.quantity = quantity;
+        this.totalCalculatedAmount = totalCalculatedAmount;
+        this.environment = environment;
+    }
+
+    @Override
+    public String toString() {
+        return "CurrencyConversion{" +
+                "id=" + id +
+                ", from='" + from + '\'' +
+                ", to='" + to + '\'' +
+                ", conversionMultiple=" + conversionMultiple +
+                ", quantity=" + quantity +
+                ", totalCalculatedAmount=" + totalCalculatedAmount +
+                ", environment='" + environment + '\'' +
+                '}';
+    }
+}
